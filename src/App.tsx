20bdd36623c5e045b464cba827ab35dc3ea1528b
chiman <rlaclghks123@naver.com>
2022-09-25 18:43:54 +0900

    framer motion 8: layout prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
@@ -19,63 +19,34 @@ const Box = styled(motion.div)`
   background-color: white;
   font-size: 30px;
   display: flex;
-  justify-content: center;
-  align-items: center;
   top: 100px;
   position: absolute;
 `;
 
-const BoxVar = {
-  entry: (isBack: boolean) => ({
-    x: isBack ? -500 : 500,
-    scale: 0,
-    opacity: 0,
-  }),
-
-  visible: {
-    x: 0,
-    scale: 1,
-    opacity: 1,
-    transition: { duration: 1 },
-  },
-  exit: (isBack: boolean) => ({
-    x: isBack ? 500 : -500,
-    scale: 0,
-    opacity: 0,
-    transition: { duration: 1 },
-  }),
-};
+const Circle = styled(motion.div)`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: tomato;
+`;
 
 function App() {
-  const [visible, setVisible] = useState(0);
-  const [isBack, setBack] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
-  const nextBtn = () => {
-    setBack(false);
-    setVisible(prev => (prev === 10 ? 1 : prev + 1));
-  };
-
-  const prevBtn = () => {
-    setBack(true);
-    setVisible(prev => (prev === 1 ? 10 : prev - 1));
+  const toggleBtn = () => {
+    setClicked(prev => !prev);
   };
 
   return (
-    <Wrapper>
-      <AnimatePresence custom={isBack}>
-        <Box
-          key={visible}
-          variants={BoxVar}
-          initial="entry"
-          animate="visible"
-          exit="exit"
-          custom={isBack}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={nextBtn}>next</button>
-      <button onClick={prevBtn}>prev</button>
+    <Wrapper onClick={toggleBtn}>
+      <Box
+        style={{
+          justifyContent: clicked ? "flex-start" : "flex-end",
+          alignItems: clicked ? "flex-start" : "flex-end",
+        }}
+      >
+        <Circle layout />
+      </Box>
     </Wrapper>
   );
 }
